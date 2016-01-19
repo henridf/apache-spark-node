7d6b055b4c9de622a9e0bbd513de35181af29081
tobilg <tobilg@users.noreply.github.com>
2016-01-19 16:56:02 +0100

    Refactoring, so that the the module can be used independently in other projects.

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 "use strict";
 
+var fs = require("fs");
+var path = require("path");
+
 var spark = require("./js");
 var functions = require("./js/functions");
 
-
 module.exports = function (args, assembly_jar) {
+
+    var args =
+        // fake presence of a main class so that SparkSubmitArguments doesn't
+        // bail. (It won't be run)
+        ["--class", "org.apache.spark.repl.Main",
+            "--name", "spark-node shell"]
+            .concat(
+                process.argv.slice(2) // remove 'node spark-node'
+            )
+            .concat(["spark-shell"]);
+
+    var assembly_jar = process.env.ASSEMBLY_JAR;
+
+    if (typeof assembly_jar != "string" || !fs.statSync(path.join(assembly_jar)).isFile()) {
+        console.error("Error: ASSEMBLY_JAR environment variable does not contain valid path");
+        process.exit(1);
+    }
+
     return {
         // don't expose sparkcontext until/if we support RDD's
         // sparkContext: spark.sparkContext(args, assembly_jar),
